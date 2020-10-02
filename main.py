b1f12048b5ca811c2cf26f7a11fa1ecbb331d8d3
PedroBertoldi <47363199+PedroBertoldi@users.noreply.github.com>
2020-10-02 09:56:23 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+import Library.dictionairs as lib_dict
+import Library.passwordGenerator as lib_pass
+import Library.Windown as lib_win
+
+if __name__ == "__main__":
+    janela = lib_win.main_windown()
+    janela.run()
